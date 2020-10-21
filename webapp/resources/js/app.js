b280c2cc6d90893b4e4f0bed5f5388d787f70abc
Pavel <pvolkv@gmail.com>
2020-10-21 22:47:16 +0300

    wip

--- webapp/resources/js/app.js
+++ webapp/resources/js/app.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import store from 'vuex'
 import router from 'vue-router'
-import App from 'components/App'
 
-Vue.config.productionTip = false
+
 
 /* eslint-disable no-new */
 new Vue({
-    store,
-    router,
-    App
-})
+        el: '#app',
+        components:
+            {
+                store,
+                router
+            }
+    }
+)
