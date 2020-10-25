db9b0491bfd1b30a6cdf75d2870233614d04d5e5
Pavel <pvolkv@gmail.com>
2020-10-25 09:49:19 +0300

    :wrench:

--- webapp/webpack.mix.js
+++ webapp/webpack.mix.js
@@ -1,5 +1,9 @@
 const mix = require('laravel-mix');
-
+require('laravel-mix-purgecss');
 
 mix.js('resources/js/app.js', 'public/js')
 mix.sass('resources/scss/app.scss', 'public/css')
+
+mix.purgeCss({
+    extensions: ['html', 'php']
+})
